8dce95fb07fb751f48d6bff1843eb2ad1c7bd251
chris7395328 <42464407+chris7395328@users.noreply.github.com>
2024-03-12 22:53:42 +0800

    Update and rename example-custom-script-1.sh to 360v6-script-1.sh

--- 360v6-script-1.sh
+++ 360v6-script-1.sh
@@ -36,3 +36,5 @@
 #EOF
 #git apply example.patch
 
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwallpackages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
