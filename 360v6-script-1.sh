55aaa442a30798720628a52e85cc220ad94913f0
chris7395328 <42464407+chris7395328@users.noreply.github.com>
2024-03-13 15:38:45 +0800

    Update 360v6-script-1.sh

--- 360v6-script-1.sh
+++ 360v6-script-1.sh
@@ -38,3 +38,4 @@
 
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git passwallpackages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
