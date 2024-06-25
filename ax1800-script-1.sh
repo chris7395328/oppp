2ce5bca0a537524c7e9ef2b187318aac62fb39cd
chris7395328 <42464407+chris7395328@users.noreply.github.com>
2024-06-25 12:59:15 +0800

    Create ax1800-script-1.sh

---
+++ ax1800-script-1.sh
@@ -0,0 +1,2 @@
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwallpackages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
